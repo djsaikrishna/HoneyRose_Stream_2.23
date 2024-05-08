a3dbb99a1cdfd228e887646b4786282414b10de4
Sai <90199989+djsaikrishna@users.noreply.github.com>
2024-05-08 23:22:45 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -26,7 +26,7 @@ class Var(object):
     HASH_LENGTH = int(environ.get("HASH_LENGTH", 6))
     if not 5 < HASH_LENGTH < 64:
         sys.exit("Hash length should be greater than 5 and less than 64")
-    FQDN = str(environ.get("FQDN", "egyptstreaming6969.onrender.com"))
+    FQDN = str(environ.get("FQDN", "stream.egywatch.site"))
     URL = "http{}://{}{}/".format(
             "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
     )
